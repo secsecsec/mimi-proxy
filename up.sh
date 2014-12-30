2b85ae40f607bb34cfa59b345cda6f60a7cbfd37
Falaleev Maxim <max@studio107.ru>
2014-12-30 16:00:52 +0300

    Cleanup

--- up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# etcdctl set /apps/u1/frontends/f1  '{"hosts": ["garant43.dev"]}'
-# etcdctl set /apps/u1/frontends/f1  '{"hosts": ["localhost"]}'
-
-etcdctl set /apps/u1/backends/b1 '{"url": "localhost:3000"}'
-# etcdctl set /apps/u1/backends/b1 '{"url": "localhost:8000"}'
