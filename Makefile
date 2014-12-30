45683cc4bca28b1da86d519f5701e73a1b844c35
Falaleev Maxim <max@studio107.ru>
2014-12-30 15:57:51 +0300

    Small update and cleanup

--- Makefile
+++ Makefile
@@ -1,2 +1,9 @@
 all:
-	go build -o slt
+	go build -o mimi-proxy
+
+run:
+    sudo ./mimi-proxy -path="./config.json"
+
+compile_run: all
+	./tmp.sh
+	sudo ./mimi-proxy -path="./config.json"
--- tmp.sh
+++ tmp.sh
@@ -7,5 +7,3 @@ etcdctl setdir /apps/u1/backends
 etcdctl setdir /apps/u1/frontends
 etcdctl set /apps/u1/backends/b1 '{"url": "localhost:8000"}'
 etcdctl set /apps/u1/frontends/f1 '{"hosts": ["localhost"]}'
-etcdctl set /apps/u2/backends/b2 '{"url": "localhost:3000"}'
-etcdctl set /apps/u2/frontends/f2  '{"hosts": ["garant43.dev"], "tls_crt": "", "tls_key": ""}'
